34637ff86b21a4b1b98a1254e109808dbeeea651
Omer-Cicek <omercicek160408@gmail.com>
2022-05-24 18:50:03 +0300

    folders created

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import { Layout, Menu } from 'antd';
+
+const { Header } = Layout;
+
+const Navbar = () => {
+  return (
+    // <Layout>
+    <Header
+      style={{
+        position: 'fixed',
+        zIndex: 1,
+        width: '100%',
+      }}
+    >
+      <div className="logo" />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        items={new Array(3).fill(null).map((_, index) => ({
+          key: String(index + 1),
+          label: `nav ${index + 1}`,
+        }))}
+      />
+    </Header>
+    // </Layout>
+  );
+};
+
+export default Navbar;
